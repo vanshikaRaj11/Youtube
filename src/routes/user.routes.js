2665171607231a2467f540df86e89d99cf928407
Vanshika Rajput <vanshikavce19@gmail.com>
2024-11-06 22:02:34 +0530

    subscription model added & more user controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,7 @@ import {
   registerUser,
   loginUser,
   logoutUser,
+ refreshAccessToken
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
@@ -27,4 +28,6 @@ router.route("/login").post(loginUser);
 
 //secured routes
 router.route("/logout").post(verifyJwt, logoutUser);
+router.route("refresh-token").post(refreshAccessToken)
+
 export default router;
