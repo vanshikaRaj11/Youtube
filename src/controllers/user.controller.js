a9e26664264e83bcedf3e2f18f73551ada04da0d
Vanshika Rajput <vanshikavce19@gmail.com>
2024-11-10 20:57:08 +0530

    Add comment, like, playlist model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -132,11 +132,12 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
+  console.log(req.user._id)
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined,
+      $unset: {
+        refreshToken: 1,
       },
     },
     {
