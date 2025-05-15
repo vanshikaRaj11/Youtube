25d4d8e55be2404255dc47821b215042163da347
Vanshika Rajput <vanshikavce19@gmail.com>
2025-05-15 19:36:42 +0530

    fix: correct typo in refresh token variable name for consistency

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -156,7 +156,8 @@ const logoutUser = asyncHandler(async (req, res) => {
 });
 
 const refreshAccessToken = asyncHandler(async (req, res) => {
-  const incomingRefreshToken = req.cookies.refrehToken || req.body.refrehToken;
+  const incomingRefreshToken =
+    req.cookies.refreshToken || req.body.refreshToken;
   if (!incomingRefreshToken) {
     throw new ApiError(401, "Unauthorized request");
   }
